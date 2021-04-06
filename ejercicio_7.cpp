6cfb9c4962bdb5a2b47efbaec30b16a90a53fff9
Humberto Rivero <63797901+danieldamian09@users.noreply.github.com>
2021-04-05 22:07:14 -0300

    Ejercicios 7 y 8
    Condicional IF

---
+++ ejercicio_7.cpp
@@ -0,0 +1,22 @@
+//Nombre:
+//TP N°:
+//EJ N°:
+//Comentarios:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numA, numB, numC;
+
+    cout << "Por favor ingrese tres numero :" << endl;
+    cin >> numA;
+    cin >> numB;
+    cin >> numC;
+
+    if(numA != numB && numA != numC && numB != numC) {
+        cout << "Todos los numeros son diferentes" << endl;
+    };
+
+	return 0;
+}
---
+++ ejercicio_8.cpp
@@ -0,0 +1,30 @@
+//Nombre:
+//TP N°:
+//EJ N°:
+//Comentarios:
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int ladoA, ladoB, ladoC;
+
+    cout << "Por favor ingrese la longitud de los tres lados del triangulo:" << endl;
+    cin >> ladoA;
+    cin >> ladoB;
+    cin >> ladoC;
+
+    if(ladoA == ladoB && ladoB == ladoC) {
+        cout << "El triangulo es Equilatero" << endl;
+    }else{
+        if(ladoA == ladoB || ladoB == ladoC) {
+            cout << "El triangulo es Isosceles" << endl;
+        }else{
+            cout << "El triangulo es Escaleno" << endl;
+        };
+    };
+
+
+	return 0;
+}
